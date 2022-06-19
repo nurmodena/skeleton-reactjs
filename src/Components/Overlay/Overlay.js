8937d34355ed948371d8ace8467fcab798a4c8a4
Muhamad Nur <muhammadnur@stmik-banisaleh.ac.id>
2022-06-19 21:07:28 +0700

    create overlay component

---
+++ src/Components/Overlay/Overlay.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Overlay({ display }) {
+    if (display) {
+        return (
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                height: '100%',
+                background: '#ffffff99',
+                zIndex: 1000,
+                top: 0,
+                left: 0, position: 'absolute'
+            }}>
+                <i class="fas fa-2x fa-sync-alt fa-spin"></i>
+                <span style={{ marginTop: 10, fontWeight: 500 }}>Processing, please wait . . .</span>
+            </div>
+        );
+    } else {
+        return '';
+    }
+}
