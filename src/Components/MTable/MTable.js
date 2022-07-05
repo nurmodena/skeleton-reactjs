90879151476d8d88bbb3ec6cafdc0257106970ff
muhamad.nur <muhamad.nur@modena.com>
2022-07-05 14:44:15 +0700

    update domain

--- src/Components/MTable/MTable.js
+++ src/Components/MTable/MTable.js
@@ -394,53 +394,57 @@ const MTable = forwardRef((props, ref) => {
           </div>
         )}
       </div>
-      <table className="table" style={{ marginTop: 16 }}>
-        <thead>
-          <tr>
-            {showIndex && (<th style={{ width: 60 }}>No</th>)}
-            {columns.map((item, i) => {
-              return item.sortable
-                ? <th key={'key-' + i} style={item.style || {}}>
-                  <div
-                    className="d-flex"
-                    style={{
-                      justifyContent: 'space-between',
-                      alignItems: 'center',
-                      cursor: 'pointer',
-                    }}
-                    onClick={onSort(item.field)}
-                  >
-                    <span className="d-block mr-2 flex-1">{item.title}</span>
-                    {paginator.order == item.field &&
-                      <i
-                        className={
-                          paginator.direction == 'asc'
-                            ? 'fa fa-arrow-down'
-                            : 'fa fa-arrow-up'
-                        }
-                      />}
-                  </div>
-                </th>
-                : <th key={'key-' + i} style={item.style || {}}>{item.title}</th>;
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, i) => (
-            <tr key={'key-' + i}>
-              {showIndex && (<td>{i + startRow}</td>)}
-              {columns.map((col, j) => {
-                return (
-                  <td key={'key_col' + j} style={col.style ? col.style : {}}>
-                    {col.render ? col.render(item) : item[col.field]}
-                  </td>
-                );
-              })}
-            </tr>
-          ))}
+      <div className="row">
+        <div className="col-lg-12 col-md-12 col-12 mtable-container">
+          <table className="table" style={{ marginTop: 16 }}>
+            <thead>
+              <tr>
+                {showIndex && (<th style={{ width: 60 }}>No</th>)}
+                {columns.map((item, i) => {
+                  return item.sortable
+                    ? <th key={'key-' + i} style={item.style || {}}>
+                      <div
+                        className="d-flex"
+                        style={{
+                          justifyContent: 'space-between',
+                          alignItems: 'center',
+                          cursor: 'pointer',
+                        }}
+                        onClick={onSort(item.field)}
+                      >
+                        <span className="d-block mr-2 flex-1">{item.title}</span>
+                        {paginator.order == item.field &&
+                          <i
+                            className={
+                              paginator.direction == 'asc'
+                                ? 'fa fa-arrow-down'
+                                : 'fa fa-arrow-up'
+                            }
+                          />}
+                      </div>
+                    </th>
+                    : <th key={'key-' + i} style={item.style || {}}>{item.title}</th>;
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item, i) => (
+                <tr key={'key-' + i}>
+                  {showIndex && (<td>{i + startRow}</td>)}
+                  {columns.map((col, j) => {
+                    return (
+                      <td key={'key_col' + j} style={col.style ? col.style : {}}>
+                        {col.render ? col.render(item) : item[col.field]}
+                      </td>
+                    );
+                  })}
+                </tr>
+              ))}
 
-        </tbody>
-      </table>
+            </tbody>
+          </table>
+        </div>
+      </div>
       <div className="mb-3 mt-3" style={{ height: 1, background: '#ccc' }} />
       <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <div style={{ flex: 1, lineHeight: 3 }}>
